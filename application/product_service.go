6d3ea40ed9d2c9fa54ac7d1e05a6eeb68b63b0e6
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-06-01 00:51:47 -0300

    feat: Implementing ProductService

---
+++ application/product_service.go
@@ -0,0 +1,53 @@
+package application
+
+type ProductService struct {
+	Persistence ProductPersistenceInterface
+}
+
+func (p *ProductService) Get(id string) (ProductInterface, error) {
+	product, err := p.Persistence.Get(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (p *ProductService) Create(name string, price float64) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Price = price
+
+	if _, err := product.IsValid(); err != nil {
+		return &Product{}, err
+	}
+	result, err := p.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+func (p *ProductService) Enable(product ProductInterface) (ProductInterface, error) {
+	err := product.Enable()
+	if err != nil {
+		return &Product{}, err
+	}
+	result, err := p.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
+func (p *ProductService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+	if err != nil {
+		return &Product{}, err
+	}
+	result, err := p.Persistence.Save(product)
+	if err != nil {
+		return &Product{}, err
+	}
+	return result, nil
+}
+
