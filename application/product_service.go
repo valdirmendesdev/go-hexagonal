05cdc655ed3d0d122c77d44079c1afaaea0bddd9
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-06-01 23:27:29 -0300

    feat: Running the integration application

--- application/product_service.go
+++ application/product_service.go
@@ -4,6 +4,12 @@ type ProductService struct {
 	Persistence ProductPersistenceInterface
 }
 
+func NewProductService(persistence ProductPersistenceInterface) *ProductService {
+	return &ProductService{
+		Persistence: persistence,
+	}
+}
+
 func (p *ProductService) Get(id string) (ProductInterface, error) {
 	product, err := p.Persistence.Get(id)
 	if err != nil {
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/valdirmendesdev/go-hexagonal/adapters/db"
+	"github.com/valdirmendesdev/go-hexagonal/application"
+)
+
 func main() {
+	dbConn, _ := sql.Open("sqlite3", "db.sqlite")
+	productDbAdapter := db.NewProductDb(dbConn)
+	service := application.NewProductService(productDbAdapter)
+	product, _ := service.Create("Meu produto", 25.90)
+
+	service.Enable(product)
+
+	fmt.Println("Funcionou!")
 
 }
