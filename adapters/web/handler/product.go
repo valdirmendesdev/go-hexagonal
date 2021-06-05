dc31345c83b218283dbc5a6dc6c5b6ef7941f7bd
Valdir Mendes <valdir.mfjesus@yahoo.com.br>
2021-06-05 11:06:57 -0300

    feat: Adding Product creation route

--- adapters/web/handler/product.go
+++ adapters/web/handler/product.go
@@ -4,14 +4,19 @@ import (
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/urfave/negroni"
+	"github.com/valdirmendesdev/go-hexagonal/adapters/dto"
 	"github.com/valdirmendesdev/go-hexagonal/application"
 	"net/http"
 )
 
-func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface)  {
-	r.Handle("/product/{id}", n.With(
+func MakeProductHandlers(r *mux.Router, n *negroni.Negroni, service application.ProductServiceInterface) {
+	r.Handle("/products/{id}", n.With(
 		negroni.Wrap(getProduct(service)),
-		)).Methods(http.MethodGet, http.MethodOptions)
+	)).Methods(http.MethodGet, http.MethodOptions)
+	r.Handle("/products", n.With(
+		negroni.Wrap(createProduct(service)),
+	)).Methods(http.MethodPost, http.MethodOptions)
+
 }
 
 func getProduct(service application.ProductServiceInterface) http.Handler {
@@ -27,6 +32,27 @@ func getProduct(service application.ProductServiceInterface) http.Handler {
 		err = json.NewEncoder(w).Encode(product)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+	})
+}
+
+func createProduct(service application.ProductServiceInterface) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		var productDTO dto.Product
+		err := json.NewDecoder(r.Body).Decode(&productDTO)
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write(jsonError(err.Error()))
+			return
+		}
+		product, err := service.Create(productDTO.Name, productDTO.Price)
+		err = json.NewEncoder(w).Encode(product)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write(jsonError(err.Error()))
 			return
 		}
 	})
